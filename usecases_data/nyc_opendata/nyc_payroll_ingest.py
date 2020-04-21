638b5fe54249985dee4e909dd15705d2a045eacc
wayne.lee <leewtai@yahoo.com>
2020-04-20 21:25:30 -0400

    nyc payroll query

---
+++ usecases_data/nyc_opendata/nyc_payroll_ingest.py
@@ -0,0 +1,31 @@
+# Calling an API from NYCOpenData
+import json
+from glob import glob
+
+import pandas as pd
+import requests
+
+cred = json.load(open('../credentials.json', 'r'))
+params = {'$limit': 50000,
+          'fiscal_year': 2019,
+          '$offset': 0}
+# 2019 is about 100MB
+for i in range(100):
+    params.update({'$offset': i * params['$limit']})
+    data = requests.get(
+        url="https://data.cityofnewyork.us/resource/k397-673e.json",
+        params=params,
+        headers={'X-App-Token': cred['nycopendata_demo_token']})
+    payroll = data.json()
+    df = pd.DataFrame(payroll)
+    df.to_csv('data/nyc2019_payroll_offset_{}.csv'.format(params['$offset']))
+    if df.shape[0] < params['$limit']:
+        print('terminating loop at {} iterations'.format(i))
+        break
+
+
+dfs = []
+file_names = glob('data/*')
+dfs = [pd.read_csv(file_name) for file_name in file_names]
+df = pd.concat(dfs)
+df.to_csv('nyc_payroll_2019.csv')
