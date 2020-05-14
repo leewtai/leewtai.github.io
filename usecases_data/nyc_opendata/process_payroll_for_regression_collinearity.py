8e10833da62989eac87a8f5b7da1795facae02e0
wayne.lee <leewtai@yahoo.com>
2020-05-14 16:00:09 -0400

    updating use cases with nyc open data

--- usecases_data/nyc_opendata/process_payroll_for_regression_collinearity.py
+++ usecases_data/nyc_opendata/process_payroll_for_regression_collinearity.py
@@ -5,15 +5,17 @@ from nltk.corpus import names
 male_names = [n.lower() for n in names.words('male.txt')]
 female_names = [n.lower() for n in names.words('female.txt')]
 
-df = pd.read_csv("nyc_payroll_2019.csv")
+df = pd.read_csv("nyc_payroll_201901.csv")
 df.first_name = df.first_name.astype(str)
 
 
 def id_gender(first_name):
     first_name = first_name.lower()
-    if first_name in male_names:
+    is_male = first_name in male_names
+    is_female = first_name in female_names
+    if is_male and not is_female:
         return 'm'
-    elif first_name in female_names:
+    elif not is_male and is_female:
         return 'f'
     else:
         return 'other'
@@ -22,12 +24,13 @@ def id_gender(first_name):
 gender = df.apply(lambda x: id_gender(x['first_name']), 1)
 gender.value_counts()
 
-k = 15
-gender.head(k)
-df.first_name.head(k)
 df['gender_from_name'] = gender
+k = 15
+df[['first_name', 'gender_from_name']]
 
-df.drop(columns=['payroll_number'],
+df.drop(columns=['Unnamed: 0', 'payroll_number',
+                 'Unnamed: 0.1', 'agency_name',
+                 'last_name'],
         inplace=True)
 
 titles = df.title_description.value_counts()
@@ -35,4 +38,4 @@ titles = df.title_description.value_counts()
 rare_title = titles.loc[titles < 50].index
 df['rare_title'] = df.apply(lambda x: x['title_description'] in rare_title, 1)
 
-df.to_csv('processed_nyc_payroll_2019.csv', index=False)
+df.to_csv('processed_nyc_payroll_201901.csv', index=False)
