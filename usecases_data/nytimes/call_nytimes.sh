f7778a326ea19abbaaebe5d135c6d9ad7843e335
Wayne Tai Lee <leewtai@yahoo.com>
2020-08-30 19:58:58 -0600

    adding hook to bash cronjob file

--- usecases_data/nytimes/call_nytimes.sh
+++ usecases_data/nytimes/call_nytimes.sh
@@ -1,5 +1,6 @@
-#!/bin/zsh
+#!/bin/bash
 
+eval "$(conda shell.bash hook)"
 conda activate web_query
-/Users/wayne.lee/miniconda3/envs/web_query/bin/python call_nytimes.py
+/home/taiphoon/miniconda3/envs/web_query/bin/python call_nytimes.py
 conda deactivate
