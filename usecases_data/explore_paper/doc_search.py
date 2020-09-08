f98e3e6dc9a022eba4adecd6a20becebac6146b1
Wayne Tai Lee <leewtai@yahoo.com>
2020-09-08 00:23:41 -0600

    trying out different plotting strategies

--- usecases_data/explore_paper/doc_search.py
+++ usecases_data/explore_paper/doc_search.py
@@ -1,8 +1,10 @@
 import json
 import logging
+from collections import Counter
 
-# import matplotlib.pyplot as plt
-# import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
 from py2neo import Graph
 
 logging.basicConfig(format="%(asctime)-15s %(message)s",
@@ -19,14 +21,41 @@ papers = graph.run((
     "                 family_name: '{family_name}',"
     "                 middle_name: '{middle_name}'}})"
     "-[a:AUTHORED]->(p:Paper)-[r:REFERENCED]->(p2:Paper)"
-    " WHERE a.year >= 2020"
-    " RETURN p2.title AS paper, count(r) AS ref_count").format(
+    " RETURN id(p) AS auth_id, p.title AS auth_title,"
+    "        id(p2) AS ref_id, p2.title AS ref_title").format(
         **author)).data()
 
-investigate = graph.run((
-    "MATCH (p1:Paper)-[:REFERENCED]->(p2:Paper)"
-    " WHERE p2.title = 'Preprint repository arxiv achieves milestone million uploads'"
-    " RETURN p1.title")).data()
+# Is there internal referencing?
+auth_ids = {p['auth_id'] for p in papers}
+ref_ids = {p['ref_id'] for p in papers}
+# Overlaps are from self-referencing, grobid's first reference is
+# the original article
+len(auth_ids) + len(ref_ids) == len(auth_ids.union(ref_ids))
+
+ref_count = Counter([p['ref_title'] for p in papers])
+paper_edges = [(p['auth_id'], p['ref_id']) for i, p in enumerate(papers)
+               if (ref_count[p['ref_title']] > 3
+                   and p['auth_id'] != p['ref_id'])]
+
+paper_graph = nx.DiGraph()
+paper_graph.add_edges_from(paper_edges)
+# auth_pos = {i: [0.1, j/len(auth_ids)] for j, i in enumerate(auth_ids)}
+# ref_pos = {i: [0.9, np.random.uniform(0, 1)]
+#            for i in ref_ids if i in paper_graph.nodes()}
+# init_pos = {**ref_pos, **auth_pos}
+pos = nx.shell_layout(
+    paper_graph,
+    nlist=[list(auth_ids), list(ref_ids.difference(auth_ids))])
+pos = nx.spring_layout(paper_graph, pos=pos, iterations=50)
+node_list = [i for i in auth_ids.union(ref_ids) if i in paper_graph.nodes()]
+node_colors = ['blue' if i in auth_ids else 'red' for i in node_list]
+
+nx.drawing.nx_pylab.draw_networkx(
+    paper_graph, pos=pos, nodelist=node_list, node_color=node_colors)
+plt.savefig('test_draw_nx.png')
+plt.close()
+
+
 # Calculate connection weights
 
 # Plot based on connections
