b597715e6eb885e38b4d493e3b02fbccf9d74832
wayne.lee <leewtai@yahoo.com>
2021-01-14 09:06:07 -0500

    adding notes for lecture

---
+++ courses/stat_computing/lectures/lec1/in_class_demo.R
@@ -0,0 +1,21 @@
+# Basic calculator operations
+
+# Order of operations
+
+# Assigning variables
+
+# Trapezoid Rule and variables
+
+# Difference between "=" and "<-"
+
+# Vectors
+
+# Properties of vectors
+
+# Calculator functions
+
+# Default values in functions
+
+# Getting help on a function
+
+# Functions on vectors
