8a490782150b34e4f473d43df249f02f8d29ad03
Wayne Tai Lee <leewtai@yahoo.com>
2021-02-13 16:52:54 -0700

    pandas default quoting can lead to errors in CSV parsing...

--- usecases_data/twitter/wrangle_twitter.py
+++ usecases_data/twitter/wrangle_twitter.py
@@ -1,3 +1,4 @@
+from csv import QUOTE_NONNUMERIC
 from collections import Counter
 from pathlib import Path
 import json
@@ -24,10 +25,18 @@ sans_retweet = [r for r in twitter if not r['text'].startswith('RT')]
 del twitter
 
 
+def twitter_symbol_conversion(token):
+    token = re.sub('^@', 'AT_', token)
+    token = re.sub('^#', 'HASHTAG_', token)
+    return token
+
+
 def process_tweet(tweet):
-    tweet = re.sub('(Washington DC|DC)', 'washington_dc', tweet)
+    tweet = re.sub('(washingtonDC|washingtondc|WashingtonDC|Washington DC|DC)',
+                   'washington_dc', tweet)
     tokens = tokenizer.tokenize(tweet.lower())
-    lwt = [stemmer.stem(token) for token in tokens
+    lwt = [twitter_symbol_conversion(stemmer.stem(token))
+           for token in tokens
            if (len(re.sub('[^\\w]', '', token)) > 2 and
                token not in stopword_list)]
     return Counter(lwt)
@@ -37,7 +46,7 @@ records = []
 counter = 0
 for tweet in sans_retweet:
     counter += 1
-    if counter % 2 == 0:
+    if counter % 3 != 0:
         continue
     text = tweet['text']
     proc_text = process_tweet(text)
@@ -47,8 +56,8 @@ for tweet in sans_retweet:
         "retweet_count": tweet['public_metrics']['retweet_count'],
         "reply_count": tweet['public_metrics']['reply_count'],
         "like_count": tweet['public_metrics']['like_count'],
-        # 'created_at': tweet['created_at'],
-        # 'tweet_body': text,
+        'created_at': tweet['created_at'],
+        'tweet_body': text,
         **dict(proc_text)}
 #     if not tweet.get('entities'):
 #         continue
@@ -68,4 +77,5 @@ df = pd.DataFrame(records)
 df.fillna(value=0, inplace=True)
 non_zero = df.apply(lambda x: (x != 0).sum(), 0)
 sdf = df.loc[:, non_zero > 50]
-sdf.to_csv("non_retweets_dc_inaug_steal.csv", index=False)
+sdf.to_csv("non_retweets_dc_inaug_steal.csv", index=False,
+           quoting=QUOTE_NONNUMERIC)
