babac2a7bfe001bd8e8a38b805ad5d2e38f88403
wayne.lee <leewtai@yahoo.com>
2021-03-21 23:09:39 -0400

    mod get alpha vantage

--- usecases_data/alpha_vantage_api/get_started.R
+++ usecases_data/alpha_vantage_api/get_started.R
@@ -5,18 +5,27 @@ library(httr)
 library(jsonlite)
 
 # This is to not expose my api_key...
-source('api_key')
-url="https://www.alphavantage.co/query"
+creds <- read_json('../credentials.json')
+api_key <- creds[['alpha_vantage_api_key']]
+url <- "https://www.alphavantage.co/query"
 
-params <- list("function"="TIME_SERIES_INTRADAY",
-               symbol="VFINX",
-               interval="60min",
+params <- list("function"="TIME_SERIES_DAILY_ADJUSTED",
+               symbol="VOO",
+               outputsize='full',
                apikey=api_key)
 response <- GET(url=url,
                 query=params)
 dat <- content(response)
-ts_names <- sub(".* ", "", names(dat[['Time Series (60min)']][[1]]))
+ts <- dat[['Time Series (Daily)']]
+ts_features <- sub("[0-9]+\\. ", "", names(ts[[1]]))
+length(ts)
 
-jsonlite::write_json(dat, "alpha_vantage_ts_intraday.json")
+split_coeff <- as.numeric(sapply(ts, function(x) x[['8. split coefficient']]))
+split_inds <- which(split_coeff != 1)
+split_days <- names(ts)[split_inds]
+adj_close <- sapply(ts, function(x) x[['5. adjusted close']])
+dates <- strptime(names(ts), '%Y-%m-%d')
 
-demo <- dat[['Time Series (60min)']][[1]]
+plot(dates, adj_close)
+
+jsonlite::write_json(dat, "alpha_vantage_voo_ts_daily.json")
