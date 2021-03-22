f3170de8125a1bd8472f66958fb9be3f7f8a0a68
Wayne Tai Lee <leewtai@yahoo.com>
2021-03-21 20:10:19 -0600

    wrangle

---
+++ usecases_data/bioRxiv_covid/wrangle.py
@@ -0,0 +1,51 @@
+from collections import Counter
+from pathlib import Path
+import json
+import re
+
+from nltk.tokenize.destructive import NLTKWordTokenizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import pandas as pd
+import numpy as np
+
+
+blacklist = stopwords.words('english')
+stemmer = PorterStemmer()
+tokenizer = NLTKWordTokenizer()
+
+
+dat_file = Path('bioRxiv.json')
+dat = json.load(dat_file.open('r'))
+freqs = []
+
+
+def clean_token(token):
+    token = re.sub("\\.+$", "", token)
+    if token.isalpha():
+        out = stemmer.stem(token)
+    elif not re.match('[a-zA-Z]', token):
+        out = 'non_alpha'
+    else:
+        out = token
+    return out
+
+
+for datum in dat:
+    doc = datum.get('rel_title') + ' ' + datum.get('rel_abs')
+    tokens = tokenizer.tokenize(doc)
+    cln_tokens = [clean_token(token) for token in tokens
+                  if token not in blacklist and
+                  (len(token) > 2 or token.isupper())]
+    freqs.append(Counter(cln_tokens))
+
+df = pd.DataFrame(freqs)
+df.fillna(value=0, inplace=True)
+wd_use = df.apply(lambda x: sum(x > 0), axis=0)
+# 4 is the 80th percentile
+np.percentile(wd_use, np.arange(0, 101, 10))
+unwanted = wd_use <= 5
+
+df.drop(columns=unwanted.index[unwanted], inplace=True)
+df.to_csv("biorxiv_word_freq.csv")
+# without cleaning non-alpha: (14180, 93480)
