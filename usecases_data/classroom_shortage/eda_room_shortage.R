87a621696430a76c742e7ff5bb96b9060f2ff6da
wayne.lee <leewtai@yahoo.com>
2021-10-24 15:06:29 +0800

    some code to do EDA on space shortages at school

--- usecases_data/classroom_shortage/eda_room_shortage.R
+++ usecases_data/classroom_shortage/eda_room_shortage.R
@@ -24,12 +24,13 @@ ggplot(seat_cap_sum, aes(fill=source, x=capacity, y=x)) + geom_bar(position="sta
 dev.off()
 
 dat <- read_excel("sequestered_use_fall16-fall21_10072021_v2.xlsx")
+
 # Why are there multiple Class_Building_Code to a Building Name?
 # e.g. LAW + GRE both map to Green Hall Law Building
 dat <- dat %>% select(-Class_Building_Code, -Student_Enrollment_Count...22)
 # Seems like you need Building_Code + Class_Room_Code to get a unique classroom
 # e.g. Class_Room_Code 101 maps to Green Hall Law Building, PRENTIS HALL, and more
-enrollment <- dat$Student_Enrollment_Count...22
+enrollment <- dat[['Student_Enrollment_Count...14']]
 capacity <- dat$Seating_Capacity_Count
 
 uniq_terms <- unique(dat$Term_Identifier)
@@ -51,27 +52,60 @@ for(ut in uniq_terms){
 
 names(dat)
 # Days are very complicated, these are requested classes (over representative of weird schedules perhaps?)
+dat <- dat[dat$Term_Identifier == 20213, ]
 day_cnts <- table(dat$Class_Days_Code)
 day_cnts[day_cnts > 10]
+enrollment <- dat[['Student_Enrollment_Count...14']]
+enroll_cats <- cut(enrollment, seq(0, 600, 20), include.lowest=FALSE)
 
 begin_cnts <- table(dat$Class_Begin_Time)
 begin_cnts[begin_cnts > 10]
 
-dat$Class_Days_Code %in% c("M", "T", "W", "TR", "F")
-plot(1, 1, type="N", xlim=c(0, 7),
-     ylim=c(0,2400))
-for(i in seq_len(nrow(dat)){
-    days <- strsplit(dat$Class_Days_Code[i], "")[[1]]
-    ybottom <- dat$Class_Begin_Time[i]
-    ytop <- dat$Class_End_Time[i]
-
+for(j in 1:4){
+    lvl <- levels(enroll_cats)[j]
+    condition <- enroll_cats == lvl
+    temp_dat <- dat[condition, ]
+    png(paste0("time_block_enrollment_", j,".png"), 500, 1000)
+    plot(1, 1, type="n", xlim=c(0, 7),
+         ylim=c(500,2400), main=paste("2021 Fall - Enrollment", lvl),
+         ylab="Time of Day", xlab="Day of Week (1=Monday)")
+    for(i in seq_len(nrow(temp_dat))){
+        days <- strsplit(temp_dat$Class_Days_Code[i], "")[[1]]
+        ybottom <- rep(temp_dat$Class_Begin_Time[i], length(days))
+        ytop <- rep(temp_dat$Class_End_Time[i], length(days))
+        days_fact <- factor(days, levels=c("M", "T", "W", "R", "F", "S"))
+        xleft <- as.numeric(days_fact) - 0.5
+        xright <- as.numeric(days_fact) + 0.5
+        rect(xleft=xleft, xright=xright, ybottom=ybottom, ytop=ytop, col="#00000011")
+    }
+    dev.off()
 }
 
-# 202003 + 20211 both have fewer than 20 (COVID)
-table(dat$Term_Identifier, dat$Term_Calendar_Year)
-table(dat$Term_Identifier, dat$Term_Code)
-table(dat$Building_Name, dat$Class_Building_Code)
-table(dat$Building_Name, dat$Class_Room_Code)
+# the subcommittee wants to see the data broken out by sizes of the rooms
 
-dat %>% filter(Term_Identifier == 20203) %>%
-    select(Building_Name, Seating_Capacity_Count, Student_Enrollment_Count...22)
+
+
+dat$popular_time <- (
+    grepl("[MTWR]", dat$Class_Days_Code) &
+    dat$Class_Begin_Time %in% c(1010, 1140, 1310, 1810))
+table(dat$Class_Days_Code[dat$popular_time])
+table(dat$Class_Days_Code[!dat$popular_time])
+enrollment <- dat[['Student_Enrollment_Count...14']]
+capacity <- dat$Seating_Capacity_Count
+
+for(pop in c(TRUE, FALSE)){
+    condition <- dat$popular_time == pop
+    temp_enroll <- enrollment[condition]
+    temp_cap <- capacity[condition]
+    enroll_cats <- cut(temp_enroll, seq(0, 600, 20), include.lowest=FALSE)
+    cap_cats <- cut(temp_cap, seq(0, 600, 20), include.lowest=FALSE)
+    temp_dat <- data.frame(enrollment=enroll_cats, capacity=cap_cats)
+    temp_sum <- aggregate(rep(1, nrow(temp_dat)), temp_dat[1], sum, drop=FALSE)
+    png(paste0("populartime_", pop,"_enroll_hist.png"), 1200, 400)
+    myplot <- ggplot(temp_sum, aes(x=enrollment, y=x)) + geom_bar(stat="identity") +
+        ggtitle(paste0("Is Popular: ", pop)) + scale_y_continuous(limits=c(0, 50))
+    print(myplot)
+    dev.off()
+}
+
+dat[enrollment > 500, c("Course_Number", "Office_Long_Name...9", "Class_Days_Code", "Class_Begin_Time")]
