b285918256a04c2934cb2cf04a8a3e264abda4ce
wayne lee <leewtai@yahoo.com>
2023-02-10 09:27:05 -0500

    updating scripts

--- usecases_data/satellite/ingest_shapefiles.py
+++ usecases_data/satellite/ingest_shapefiles.py
@@ -6,18 +6,34 @@ import pandas as pd
 import geopandas as gpd
 from bs4 import BeautifulSoup
 
-
-with open('/home/taiphoon/Downloads/Mayan Languages of Guatemala_ Interactive (EN) map_files/Dashboard1.html', 'r') as f:
+# https://public.tableau.com/app/profile/eric.deluca/viz/GuatemalaMayanlanguagesmap/Dashboard1
+with open('data/Dashboard1.html', 'r') as f:
     html = '\n'.join(f.readlines())
 
 soup = BeautifulSoup(html)
 lang_nodes = soup.find_all('div', 'tab-vizHeaderWrapper')
 langs = [node.text for node in lang_nodes]
 
-df = gpd.read_file('/home/taiphoon/Downloads/gtm_adm_ocha_conred_2019_SHP/gtm_admbnda_adm1_ocha_conred_20190207.shp')
-df.shape
-sdf = df.loc[:, ['ADM1_ES', 'geometry']]
+gdf = gpd.read_file('data/gtm_adm_ocha_conred_2019_SHP/gtm_admbnda_adm1_ocha_conred_20190207.shp')
+gdf.shape
+sdf = gdf.loc[:, ['ADM1_ES', 'geometry']]
 sdf.head(3)
 sdf.ADM1_ES.to_numpy()
 
-pd.DataFrame(columns=sdf.ADM1_ES, index=langs).to_csv('mayan_lang_loss.csv', quoting=csv.QUOTE_NONNUMERIC)
+pd.DataFrame(columns=sdf.ADM1_ES, index=langs).to_csv('data/mayan_lang_loss.csv', quoting=csv.QUOTE_NONNUMERIC)
+
+# https://docs.google.com/spreadsheets/d/18kB5OyebrS-iMg8TS9i6a6__SMkVy93TJWSr5MO2_wM/edit?usp=sharing
+df = pd.read_csv('data/mayan_language_count.csv')
+df.rename(columns={'Unnamed: 0': 'Language'}, inplace=True)
+
+assert (df.Language == langs).all()
+dft = df.T.iloc[1:, :]
+dft.head(3)
+dft.columns = langs
+dft = dft.reset_index()
+dft.rename(columns={'index': 'ADM1_ES'}, inplace=True)
+sdf = sdf.merge(dft, on='ADM1_ES')
+sdf.head(3)
+type(sdf)
+
+sdf.to_csv('data/mayan_language_map.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
