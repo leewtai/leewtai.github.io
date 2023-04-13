af79d67e286dac4f2c5aeec898c6f670d050d99f
wayne lee <leewtai@yahoo.com>
2023-04-13 15:41:26 -0400

    getting north charleston polygons

--- usecases_data/selenium/get_arc_polygons.py
+++ usecases_data/selenium/get_arc_polygons.py
@@ -4,8 +4,6 @@ import brotli
 from time import sleep
 from seleniumwire import webdriver
 
-import requests
-
 
 driver = webdriver.Firefox()
 driver.get("https://arc.northcharleston.org/Neighbors/Index.html")
@@ -14,13 +12,13 @@ sleep(5)
 neighbor_dd = driver.find_element("id", "neighborDD")
 options = neighbor_dd.find_elements("tag name", "option")
 
+# Click on the different options in the dropdown
 for option in options:
     if option.text == 'Select One':
         continue
     option.click()
     sleep(2)
 
-reqs = driver.requests
 
 out = {}
 for req in reqs:
@@ -32,13 +30,16 @@ for req in reqs:
     bod = brotli.decompress(req.response.body)
     dat = json.loads(bod)
     feats = dat.get("features")
-    out.update({neigh: feats})
+    out.update({neigh: {'features': feats,
+                        'spatialReference': dat.get('spatialReference')}})
     # for feat in feats:
     #     geom = feat.get("geometry")
     #     neighbor = feat.get("attributes").get('NEIGHBOR')
 
-len(out)
-set(out.keys()).difference({o.text for o in options})
-{o.text for o in options}.difference(set(out.keys()))
 out.get('WINDSOR')
+bad_opt = "Select One"
+if bad_opt in out:
+    out.pop(bad_opt)
 
+json.dump(out,
+          open('north_charleston_sc_neighbhoods_polygons.json', 'w'))
