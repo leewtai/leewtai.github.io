aee8346a2474814ebcaddeb8a7aa5373da13ee10
leewtai <leewtai@yahoo.com>
2024-09-17 15:09:47 -0400

    adding 311 complaints to the same payroll ingestion

--- usecases_data/nyc_opendata/nyc_payroll_ingest.py
+++ usecases_data/nyc_opendata/nyc_payroll_ingest.py
@@ -7,31 +7,35 @@ import pandas as pd
 import requests
 
 cred = json.load(open('../credentials.json', 'r'))
-params = {'fiscal_year': 2021,
+# The comments allow us to alter betrween 
+params = {#'fiscal_year': 2023,
           '$limit': 50000,
           '$offset': 0,
           #'$where': 'agency_start_date between "2020-01-01" and "2020-01-31"'
-          '$where': 'agency_name = "POLICE DEPARTMENT"'
+          #'$where': 'agency_name = "POLICE DEPARTMENT"'
+          '$where': '(created_date between "2023-01-01" and "2023-12-31") AND (complaint_type LIKE "%Noise%" OR complaint_type LIKE "%Graffiti%")'
           }
-# 2020 is about 100MB
 for i in range(100):
     params.update({'$offset': i * params['$limit']})
     data = requests.get(
-        url="https://data.cityofnewyork.us/resource/k397-673e.json",
+        url="https://data.cityofnewyork.us/resource/erm2-nwe9.json", # 311
+        # url="https://data.cityofnewyork.us/resource/k397-673e.json", # Payroll
         params=params,
         headers={'X-App-Token': cred['nycopendata_demo_token']})
     payroll = data.json()
+    payroll[100]['complaint_type']
     df = pd.DataFrame(payroll)
-    out_fn = f'data/nyc{params["fiscal_year"]}_payroll_offset_{params["$offset"]}.csv'
-    df.to_csv(out_fn, index=False, quoting=csv.QUOTE_NONNUMERIC)
+    # out_fn = f'data/nyc{params["fiscal_year"]}_311_offset_{params["$offset"]}.csv'
+    out_fn = f'data/nyc_311_offset_{params["$offset"]}.csv'
+    df[['complaint_type', 'created_date', 'incident_zip']].to_csv(out_fn, index=False, quoting=csv.QUOTE_NONNUMERIC)
     if df.shape[0] < params['$limit']:
         print(f'terminating loop at {i} iterations')
         break
 
 
 dfs = []
-file_names = glob(f'data/*{params["fiscal_year"]}*')
+file_names = glob(f'data/nyc_311*.csv')
 dfs = [pd.read_csv(file_name) for file_name in file_names]
 df = pd.concat(dfs)
-df.to_csv(f'nyc_payroll_{params["fiscal_year"]}.csv',
+df.to_csv(f'nyc_311_noise_graffiti_2023.csv',
           index=False, quoting=csv.QUOTE_NONNUMERIC)
